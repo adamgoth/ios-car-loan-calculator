aa73c687c133d0772ee84dbaf320d37035ca9337
Adam Goth <adamgoth@gmail.com>
2016-09-03 17:31:03 -0600

    Initial Commit

---
+++ car-loan-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  car-loan-calculator
+//
+//  Created by Adam Goth on 9/3/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
